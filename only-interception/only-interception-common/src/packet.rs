d34b1c89b8d4118cd8913c0622e5032ec03aa1f3
emanuele-em <micheletti.emanuele@hotmail.com>
2024-05-15 09:05:43 +0200

    packet from kernel to user

--- only-interception/only-interception-common/src/packet.rs
+++ only-interception/only-interception-common/src/packet.rs
@@ -4,7 +4,7 @@ pub const BUF_SIZE: usize = 1500;
 #[repr(C)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Packet {
-    pub buf: [u8; BUF_SIZE], // Assuming maximum Ethernet frame size
+    pub buf: [u8; BUF_SIZE],
 }
 
 #[cfg(feature = "user")]
@@ -19,30 +19,13 @@ impl std::fmt::Display for Packet {
         let ethertype = u16::from_be_bytes([self.buf[12], self.buf[13]]);
         let payload = &self.buf[14..];
 
-        let payload_str: String = payload.iter().map(|&c| if c.is_ascii() && c != 0 { c as char } else { '.' }).collect();
-
         write!(
             f,
             "Dest MAC: {:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}, Src MAC: {:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}, EtherType: {:04x}, Payload: {:?}",
             dest_mac[0], dest_mac[1], dest_mac[2], dest_mac[3], dest_mac[4], dest_mac[5],
             src_mac[0], src_mac[1], src_mac[2], src_mac[3], src_mac[4], src_mac[5],
             ethertype,
-            payload_str
+            payload
         )
-        // Convert 'comm' and 'buf' fields to strings for display.
-        // 'String::from_utf8_lossy' will replace invalid UTF-8 sequences with U+FFFD REPLACEMENT CHARACTER.
-        // let data_str = std::str::from_utf8(&self.buf[..BUF_SIZE as usize]).unwrap();
-        // for byte in self.buf.iter() {
-        //     write!(f, "{:x}", byte)?;
-        // }
-        // Ok(())
-
-        // Write the formatted output to the formatter.
-        // Adjust formatting as needed for clarity and readability.
-        // write!(
-        //     f,
-        //     "Data: {}",
-        //     data_str
-        // )
     }
 }
--- only-interception/only-interception-ebpf/src/main.rs
+++ only-interception/only-interception-ebpf/src/main.rs
@@ -1,15 +1,30 @@
 #![no_std]
 #![no_main]
 
-use core::mem;
 use aya_ebpf::{macros::classifier, macros::map, programs::TcContext, maps::PerfEventArray, maps::PerCpuArray, bindings::TC_ACT_PIPE};
 use aya_log_ebpf::info;
 use only_interception_common::{Packet, BUF_SIZE};
 
+fn load_packet(ctx: TcContext) -> Result<(), i32> {
+    let mut len = ctx.len() as usize;
+    unsafe {
+        let packet = {
+            let ptr = PACKETS.get_ptr_mut(0).ok_or(0_i32)?;
+            &mut *ptr
+        };
+        if len >= BUF_SIZE {
+            len = BUF_SIZE;
+        }
+        for i in 0..len as usize {
+            packet.buf[i] = ctx.load(i).map_err(|_| 0)?;
+        }
+        EVENTS.output(&ctx, &(*packet), 0);
+    }
+    Ok(())
+}
 
 
 #[map]
-// static PACKETS: RingBuf = RingBuf::with_byte_size(10*mem::size_of::<Packet>() as u32, 0);
 static mut PACKETS: PerCpuArray<Packet> = PerCpuArray::with_max_entries(1, 0);
 #[map]
 static mut EVENTS: PerfEventArray<Packet> = PerfEventArray::with_max_entries(1024, 0);
@@ -25,29 +40,7 @@ pub fn only_interception_egress(ctx: TcContext) -> i32 {
 
 fn try_only_interception_egress(ctx: TcContext) -> Result<i32, i32> {
     info!(&ctx, "received an egress packet");
-    let start = ctx.data();
-    let end = ctx.data_end();
-    let mut len = ctx.len() as usize;
-
-    info!(&ctx, "packet length: {}", len);
-    // let mut tmp = [0u8; BUF_SIZE];
-    if len >= BUF_SIZE {
-        len = BUF_SIZE;
-    }
-    let packet = unsafe {
-        let ptr = PACKETS.get_ptr_mut(0).ok_or(0_i32)?;
-        &mut *ptr
-    };
-    for i in 0..len as usize {
-        packet.buf[i] = ctx.load(i).map_err(|_| 0)?;
-    }
-
-    // packet.buf = tmp;
-
-    unsafe{
-        EVENTS.output(&ctx, &(*packet), 0);
-    }
-
+    load_packet(ctx).map_err(|_| 0)?;
     Ok(TC_ACT_PIPE)
 }
 
@@ -62,7 +55,8 @@ pub fn only_interception_ingress(ctx: TcContext) -> i32 {
 
 fn try_only_interception_ingress(ctx: TcContext) -> Result<i32, i32> {
     info!(&ctx, "received an ingress packet");
-    Ok(0)
+    load_packet(ctx).map_err(|_| 0)?;
+    Ok(TC_ACT_PIPE)
 }
 
 #[panic_handler]
