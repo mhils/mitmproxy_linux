dda2a7c92531fa884bfe597bd9a5bb85626da4ae
Maximilian Hils <git@maximilianhils.com>
2024-10-14 23:48:50 +0200

    cleanup

--- echo-server/echo-test-ebpf/src/main.rs
+++ echo-server/echo-test-ebpf/src/main.rs
@@ -14,9 +14,6 @@ use aya_ebpf::programs::sk_buff::SkBuff;
 use aya_log_ebpf::{error, info};
 use echo_test_common::SockKey;
 
-#[map]
-static TEST_MAP: SockHash<SockKey> = SockHash::<SockKey>::with_max_entries(1024, 0);
-
 pub fn command_to_str(command: &[u8; 16]) -> &str {
     let len = command.iter()
         .position(|&c| c == b'\0')
@@ -25,56 +22,18 @@ pub fn command_to_str(command: &[u8; 16]) -> &str {
 }
 
 #[cgroup_sock(sock_create)]
-pub fn cgroup_sock_create(ctx: SockContext) -> i32 {
+pub fn cgroup__sock_create(ctx: SockContext) -> i32 {
     let c = ctx.command().unwrap_or_default();
     let cmd = command_to_str(&c);
 
     if cmd == "nc" {
         info!(&ctx, "sock create: {} {}", cmd, unsafe { (*ctx.sock).state });
-
         unsafe {
             (*ctx.sock).bound_dev_if = 143;  // Replace with interface id from `ip link show`
         }
     }
     1
 }
-
-
-/*
-These here all require things to be attached to the map, doesn't work for us.
-Also aya only has stream verdicts, we want to do UDP as well.
-
-#[map(name = "INGRESS")]
-static mut INGRESS: SockHash<u32> = SockHash::with_max_entries(2048, BPF_ANY);
-
-#[stream_parser]
-fn stream_parser(ctx: SkBuffContext) -> u32 {
-    ctx.len()
-}
-
-#[stream_verdict]
-fn stream_verdict(ctx: SkBuffContext) -> u32 {
-    let c = ctx.command().unwrap_or_default();
-    let cmd = command_to_str(&c);
-    info!(&ctx, "stream_verdict {} port={}", cmd, ctx.skb.remote_port());
-    // let _ret = unsafe { INGRESS.redirect_skb(&ctx, &mut key, 0) };
-    SK_PASS
-}
-*/
-
-#[sk_msg]
-pub fn echo_test(ctx: SkMsgContext) -> u32 {
-    match try_echo_test(ctx) {
-        Ok(ret) => ret,
-        Err(ret) => ret,
-    }
-}
-
-fn try_echo_test(ctx: SkMsgContext) -> Result<u32, u32> {
-    info!(&ctx, "received a message on the socket");
-    Ok(SK_PASS)
-}
-
 
 #[cfg(not(test))]
 #[panic_handler]
--- echo-server/echo-test/src/main.rs
+++ echo-server/echo-test/src/main.rs
@@ -14,103 +14,26 @@ use tokio::signal;
 async fn main() -> anyhow::Result<()> {
     env_logger::init();
 
-    /*
-    let listener = TcpListener::bind("127.0.0.1:8000")?;
-    let local_addr = listener.local_addr()?;
-     */
-
-    // Bump the memlock rlimit. This is needed for older kernels that don't use the
-    // new memcg based accounting, see https://lwn.net/Articles/837122/
-    let rlim = libc::rlimit {
-        rlim_cur: libc::RLIM_INFINITY,
-        rlim_max: libc::RLIM_INFINITY,
-    };
-    let ret = unsafe { libc::setrlimit(libc::RLIMIT_MEMLOCK, &rlim) };
-    if ret != 0 {
-        debug!("remove limit on locked memory failed, ret is: {}", ret);
-    }
-
-    // This will include your eBPF object file as raw bytes at compile-time and load it at
-    // runtime. This approach is recommended for most real-world use cases. If you would
-    // like to specify the eBPF program at runtime rather than at compile-time, you can
-    // reach for `Bpf::load_file` instead.
     let mut ebpf = aya::Ebpf::load(aya::include_bytes_aligned!(concat!(
         env!("OUT_DIR"),
         "/echo-test"
     )))?;
+
     if let Err(e) = aya_log::EbpfLogger::init(&mut ebpf) {
         // This can happen if you remove all log statements from your eBPF program.
         warn!("failed to initialize eBPF logger: {}", e);
     }
 
-    let prog: &mut CgroupSock = ebpf.program_mut("cgroup_sock_create").unwrap().try_into()?;
+    let prog: &mut CgroupSock = ebpf.program_mut("cgroup__sock_create").unwrap().try_into()?;
+    // root cgroup to get all events.
     let cgroup = std::fs::File::open("/sys/fs/cgroup/")?;
     prog.load()?;
-    prog.attach(&cgroup, CgroupAttachMode::Single)?;
-    println!("Attached!");
-
-    let mut sock_map: SockHash<_, SockKey> = ebpf.map("TEST_MAP").unwrap().try_into()?;
-
-    let key = SockKey {
-        remote_ip4: 0,
-        local_ip4: 0,
-        remote_port: 0,
-        local_port: 0,
-    };
-
-
-    let map_fd = sock_map.fd().try_clone()?;
-
-    let prog: &mut SkMsg = ebpf.program_mut("echo_test").unwrap().try_into()?;
-    prog.load()?;
-    prog.attach(&map_fd)?;
-
-    /*
-    info!("Listening on {local_addr}");
-
-    thread::spawn(move || {
-        for stream in listener.incoming() {
-            match stream {
-                Ok(stream) => {
-
-                    /*
-                    let mut sock_map: SockHash<_, SockKey> = ebpf.map_mut("TEST_MAP").unwrap().try_into().expect("cannot get map");
-                    sock_map.insert(key, stream.as_raw_fd(), 0).expect("cannot insert");
-                    */
-                    thread::spawn(move || {
-                        handle_client(stream);
-                    });
-                }
-                Err(_) => {
-                    println!("Error");
-                }
-            }
-        }
-    });
-     */
+    prog.attach(&cgroup, CgroupAttachMode::AllowMultiple)?;
+    info!("Attached!");
 
     info!("Waiting for Ctrl-C...");
     signal::ctrl_c().await?;
     info!("Exiting...");
 
     Ok(())
-}
-
-fn handle_client(mut stream: TcpStream) {
-    loop {
-        let mut read = [0; 1028];
-        match stream.read(&mut read) {
-            Ok(n) => {
-                println!("echo: {:?}", &read[0..n]);
-                if n == 0 {
-                    // connection was closed
-                    break;
-                }
-                stream.write(&read[0..n]).unwrap();
-            }
-            Err(err) => {
-                panic!("{}", err);
-            }
-        }
-    }
 }
