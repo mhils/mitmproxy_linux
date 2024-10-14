5bfa8f562e45bd02d50beb3d344d9c57ad6c3da7
emanuele-em <micheletti.emanuele@hotmail.com>
2024-10-14 20:07:52 +0200

    poc without tap

--- only-interception/only-interception/src/main.rs
+++ only-interception/only-interception/src/main.rs
@@ -6,17 +6,31 @@ use aya_log::BpfLogger;
 use clap::Parser;
 use log::{debug, info, warn, error};
 use tokio::signal;
+use std::thread;
 use only_interception_common::{Packet, BUF_SIZE};
 use aya::maps::AsyncPerfEventArray;
 use bytes::BytesMut;
 use tun_tap::{Iface, Mode};
 use std::sync::{Arc, Mutex};
 use hex;
+use std::process::Command;
+use std::time::Duration;
 
 #[derive(Debug, Parser)]
 struct Opt {
     #[clap(short, long, default_value = "eth0")]
     iface: String,
+}
+
+/// Run a shell command. Panic if it fails in any way.
+fn cmd(cmd: &str, args: &[&str]) {
+    let ecode = Command::new("ip")
+        .args(args)
+        .spawn()
+        .unwrap()
+        .wait()
+        .unwrap();
+    assert!(ecode.success(), "Failed to execte {}", cmd);
 }
 
 #[tokio::main]
@@ -78,12 +92,52 @@ async fn main() -> Result<(), anyhow::Error> {
     let mut perf_array = AsyncPerfEventArray::try_from(bpf.take_map("EVENTS").unwrap())?;
     let len_of_packet = std::mem::size_of::<Packet>();
     //Create tap interface
-    let tap = Arc::new(Mutex::new(Iface::new("tap10", Mode::Tap).unwrap()));
+    // let iface = Iface::new("tap10", Mode::Tap).unwrap();
+    // eprintln!("Iface: {:?}", iface);
+    // Configure the „local“ (kernel) endpoint. Kernel is (the host) 10.107.1.3, we (the app)
+    // pretend to be 10.107.1.2.
+    // cmd("ip", &["addr", "add", "dev", iface.name(), "10.107.1.3/24"]);
+    // cmd("ip", &["link", "set", "up", "dev", iface.name()]);
+    
+    // const PING: &[u8] = &[0, 0, 8, 0, 69, 0, 0, 84, 44, 166, 64, 0, 64, 1, 247, 40, 10, 107, 1, 2, 10,
+    // 107, 1, 3, 8, 0, 62, 248, 19, 160, 0, 2, 232, 228, 34, 90, 0, 0, 0, 0, 216, 83, 3, 0, 0, 0, 0,
+    // 0, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
+    // 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55];
+    //
+    // let iface = Arc::new(iface);
+    // let iface_writer = Arc::clone(&iface);
+    // let iface_reader = Arc::clone(&iface);
+    // let writer = thread::spawn(move || {
+    //     // Yeh, mutable reference to immutable thing. Nuts…
+    //     loop {
+    //         thread::sleep(Duration::from_secs(1));
+    //         println!("Sending a ping");
+    //         let amount = iface_writer.send(PING).unwrap();
+    //         assert!(amount == PING.len());
+    //     }
+    // });
+    // let reader = thread::spawn(move || {
+        // MTU + TUN header
+    //     let mut buffer = vec![0; 1504];
+    //     loop {
+    //         let size = iface_reader.recv(&mut buffer).unwrap();
+    //         // Strip the „header“
+    //         assert!(size >= 4);
+    //         println!("Packet: {:?}", &buffer[4..size]);
+    //     }
+    // });
+    // writer.join()
+    //     .unwrap();
+    // reader.join()
+    //     .unwrap();
+
+    // let iface = Arc::new(Mutex::new(iface));
+
+
 
     for cpu_id in online_cpus()? {
         // open a separate perf buffer for each cpu
         let mut buf = perf_array.open(cpu_id, Some(32))?;
-        let tap_clone = Arc::clone(&tap);
 
         // process each perf buffer in a separate task
         tokio::spawn(async move {
@@ -106,12 +160,6 @@ async fn main() -> Result<(), anyhow::Error> {
                     let buf = &mut buffers[i];
                     let hex_string = hex::encode(&buf);
                     info!("hex string: {}", hex_string);
-
-                    let tap = tap_clone.lock().unwrap();
-                    match tap.send(&buf[..]) {
-                        Ok(_) => info!("Packet sent to tap"),
-                        Err(e) => error!("Error sending packet to tap: {}", e),
-                    }
                 }
             }
         });
